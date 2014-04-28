d90bc29ef975806962d63179d12c0571560c9588
Christian Ruiz <christian.ruiz.mail@gmail.com>
2014-04-28 19:14:25 +0200

    #8 Added Windows Phone and Windows Store support

---
+++ Chance.MvvmCross.Plugins.UserInteraction.WindowsPhone/UserInteractionPluginBootstrap.cs
@@ -0,0 +1,11 @@
+using Cirrious.CrossCore.Plugins;
+
+namespace <YourNamespace>.Bootstrap
+{
+	public class UserInteractionPluginBootstrap 
+		: MvxLoaderPluginBootstrapAction<Chance.MvvmCross.Plugins.UserInteraction.PluginLoader, Chance.MvvmCross.Plugins.UserInteraction.WindowsPhone.Plugin>
+
+	{
+	}
+}
+
